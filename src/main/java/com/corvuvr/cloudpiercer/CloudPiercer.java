d9e18c418a584089de196edfd5557bc276cac8fc
Corvuvr <d.podgornyy@inbox.ru>
2026-01-29 18:09:22 +0300

    Rename classname

--- src/main/java/com/corvuvr/cloudpiercer/CloudPiercer.java
+++ src/main/java/com/corvuvr/cloudpiercer/CloudPiercer.java
@@ -15,15 +15,15 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(ExampleMod.MODID)
-public class ExampleMod
+@Mod(CloudPiercer.MODID)
+public class CloudPiercer
 {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "cloudpiercer";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
-    public ExampleMod(FMLJavaModLoadingContext context)
+    public CloudPiercer(FMLJavaModLoadingContext context)
     {
         IEventBus modEventBus = context.getModEventBus();
 
